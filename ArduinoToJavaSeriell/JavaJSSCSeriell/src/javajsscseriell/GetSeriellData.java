a96c08146a970d07eb4f5da1d9549e1f2bcc6e78
RobinThorholm <rocio944@gmail.com>
2018-10-31 16:36:16 +0100

    Cleaned up some code in java serial communication

---
+++ ArduinoToJavaSeriell/JavaJSSCSeriell/src/javajsscseriell/GetSeriellData.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javajsscseriell;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+/**
+ *
+ * @author rocio
+ */
+public class GetSeriellData
+{
+
+    public String[] getAvailableComPorts()
+    {
+        // getting serial ports list into the array
+
+        String[] portNames = SerialPortList.getPortNames();
+
+        if (portNames.length == 0)
+        {
+            System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
+            System.out.println("Press Enter to exit...");
+
+            try
+            {
+                System.in.read();
+            } catch (Exception e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < portNames.length; i++)
+        {
+            System.out.println(portNames[i]);
+        }
+        return portNames;
+    }
+
+    public HashMap readSerialData(String comPort, int baudRate)
+    {
+
+        HashMap<String, String> SerialDataList;
+        SerialDataList = new HashMap<String, String>();
+
+        boolean receivedData = false;
+        //Declare Special Symbol Used in Serial Data Stream from Arduino
+        String start_char = "<";
+        String end_char = ">";
+        String sep_char = ":";
+        //Define Serial Port # -- can be found in Device Manager or Arduino IDE
+        SerialPort serialPort = new SerialPort(comPort);
+
+        try
+        {
+            serialPort.openPort();
+        } catch (SerialPortException ex)
+        {
+            System.out.println(ex);
+        }
+
+        while (receivedData == false)
+        {
+            byte[] buffer = null;
+            try
+            {
+                //serialPort.setParams(9600, 8, 1, 0);
+
+                serialPort.setParams(baudRate, 8, 1, 0);
+
+                buffer = serialPort.readBytes();
+
+                try
+                {
+                    Thread.sleep(250);
+                } catch (InterruptedException ex)
+                {
+                    Thread.currentThread().interrupt();
+                }
+
+                String dataStream = new String(buffer);
+
+                dataStream = dataStream.substring(dataStream.indexOf(start_char) + 1);
+                dataStream = dataStream.substring(0, dataStream.indexOf(end_char));
+                dataStream = dataStream.replace("?", "");
+                String[] data = dataStream.split(sep_char);
+
+                for (int i = 0; i < data.length; i = i + 2)
+                {
+                    SerialDataList.put(data[i], data[i + 1]);
+                    System.out.println("Key: " + data[i] + "     Value:" + data[i + 1]);
+//                    receivedData = true;
+
+                }
+
+            } catch (Exception ex)
+            {
+                System.out.println(ex);
+            }
+
+        }
+        try
+        {
+            serialPort.closePort();
+        } catch (Exception e)
+        {
+            System.err.println(e);
+        }
+
+        return SerialDataList;
+    }
+}
--- ArduinoToJavaSeriell/JavaJSSCSeriell/src/javajsscseriell/JavaJSSCSeriell.java
+++ ArduinoToJavaSeriell/JavaJSSCSeriell/src/javajsscseriell/JavaJSSCSeriell.java
@@ -5,6 +5,7 @@
  */
 package javajsscseriell;
 
+import java.util.HashMap;
 import jssc.*;
 
 /**
@@ -20,74 +21,86 @@ public class JavaJSSCSeriell
     public static void main(String[] args)
     {
 
-        // getting serial ports list into the array
-        String[] portNames = SerialPortList.getPortNames();
+        HashMap serialData = new GetSeriellData().readSerialData("com4", 57600);
+        System.out.println("Data is gotten");
 
-        if (portNames.length == 0)
-        {
-            System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
-            System.out.println("Press Enter to exit...");
-            try
-            {
-                System.in.read();
-            } catch (Exception e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return;
-        }
-
-        for (int i = 0; i < portNames.length; i++)
-        {
-            System.out.println(portNames[i]);
-        }
-
-        //Declare Special Symbol Used in Serial Data Stream from Arduino
-        String start_char = "<";
-        String end_char = ">";
-        String sep_char = ":";
-        //Define Serial Port # -- can be found in Device Manager or Arduino IDE
-        SerialPort serialPort = new SerialPort("COM3");
-        try
-        {
-            serialPort.openPort();
-        } catch (SerialPortException ex)
-        {
-            System.out.println(ex);
-        }
-
-        while (true)
-        {
-            try
-            {
-
-                serialPort.setParams(9600, 8, 1, 0);
-                byte[] buffer;
-                buffer = serialPort.readBytes(24);
-                
-
-                String dataStream = new String(buffer);
-                dataStream = dataStream.substring(dataStream.indexOf(start_char) + 1);
-                dataStream = dataStream.substring(0, dataStream.indexOf(end_char));
-                dataStream = dataStream.replace("?", "");
-                String[] data = dataStream.split(sep_char);
-
-                for (int i = 0; i < data.length; i++)
-                {
-                    System.out.println(data[i]);
-                  
-
-                }
-
-            } catch (SerialPortException ex)
-            {
-                System.out.println(ex);
-
-            }
-
-        }
-
+//        // getting serial ports list into the array
+//        String[] portNames = SerialPortList.getPortNames();
+//
+//        if (portNames.length == 0)
+//        {
+//            System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
+//            System.out.println("Press Enter to exit...");
+//            try
+//            {
+//                System.in.read();
+//            } catch (Exception e)
+//            {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//            return;
+//        }
+//
+//        for (int i = 0; i < portNames.length; i++)
+//        {
+//            System.out.println(portNames[i]);
+//        }
+//
+//        //Declare Special Symbol Used in Serial Data Stream from Arduino
+//        String start_char = "<";
+//        String end_char = ">";
+//        String sep_char = ":";
+//        //Define Serial Port # -- can be found in Device Manager or Arduino IDE
+//        SerialPort serialPort = new SerialPort("COM1");
+//        try
+//        {
+//            serialPort.openPort();
+//        } catch (SerialPortException ex)
+//        {
+//            System.out.println(ex);
+//        }
+//
+//        while (true)
+//        {
+//            try
+//            {
+//
+//                //serialPort.setParams(9600, 8, 1, 0);
+//                serialPort.setParams(57600, 8, 1, 0);
+//                byte[] buffer;
+//                buffer = serialPort.readBytes(25);
+//
+//                String dataStream = new String(buffer);
+//                dataStream = dataStream.substring(dataStream.indexOf(start_char) + 1);
+//                dataStream = dataStream.substring(0, dataStream.indexOf(end_char));
+//                dataStream = dataStream.replace("?", "");
+//                String[] data = dataStream.split(sep_char);
+//
+//                for (int i = 0; i < data.length; i++)
+//                {
+//                    System.out.println(data[i]);
+//
+//                }
+//
+//                
+//            } catch (Exception ex )
+//            {
+//                System.out.println(ex);
+//              
+//            }
+//            try
+//            {
+//                Thread.sleep(100);
+//            } catch (InterruptedException ex)
+//            {
+//                Thread.currentThread().interrupt();
+//            }
+//            
+//            
+//
+//        }
+//
+//    }
     }
-
 }
