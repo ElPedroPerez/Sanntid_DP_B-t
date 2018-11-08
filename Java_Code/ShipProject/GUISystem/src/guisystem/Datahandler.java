73834dd5426c00ffa152b79dde6bc41139428344
Bjørnar T <bmtennfjord@gmail.com>
2018-11-08 20:52:07 +0100

    Fixed package names for guisystem in DataHandler, UDPReciever and UDPSender

--- Java_Code/ShipProject/GUISystem/src/guisystem/Datahandler.java
+++ Java_Code/ShipProject/GUISystem/src/guisystem/Datahandler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package remotecontroller;
+package guisystem;
 
 import java.math.BigInteger;
 import java.util.Arrays;
